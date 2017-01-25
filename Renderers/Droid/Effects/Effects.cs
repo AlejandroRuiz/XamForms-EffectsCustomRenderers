b372cd5ff21a77fc9b2d9c206f35e1257d41523c
Alejandro Ruiz Varela <elgoberlivel@gmail.com>
2017-01-25 14:51:12 -0600

    [First Commit]

---
+++ Renderers/Droid/Effects/Effects.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.Graphics.Drawables;
+using Android.Views;
+using Android.Widget;
+using Renderers.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//[assembly: ResolutionGroupName("AwesomeEffects")]
+//[assembly: ExportEffect(typeof(NoClickableListViewEffect), "NoClickableListViewEffect")]
+namespace Renderers.Droid
+{
+	public class NoClickableListViewEffect : PlatformEffect
+	{
+		#region Effect Life Cycle
+
+		protected override void OnAttached()
+		{
+			var listView = (global::Android.Widget.ListView)Control;
+			listView.OnItemClickListener = null;
+		}
+
+		protected override void OnDetached()
+		{
+			var listView = (global::Android.Widget.ListView)Control;
+		}
+
+		#endregion
+	}
+}
---
+++ Renderers/iOS/Effects/Effects.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Renderers.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+//[assembly: ResolutionGroupName("AwesomeEffects")]
+//[assembly: ExportEffect(typeof(NoClickableListViewEffect), "NoClickableListViewEffect")]
+namespace Renderers.iOS
+{
+	public class NoClickableListViewEffect : PlatformEffect
+	{
+		#region Effect Life Cycle
+
+		protected override void OnAttached()
+		{
+			var listView = (UITableView)Control;
+			listView.AllowsSelection = false;
+		}
+
+		protected override void OnDetached()
+		{
+			var listView = (UITableView)Control;
+			listView.AllowsSelection = true;
+		}
+
+		#endregion
+	}
+}
